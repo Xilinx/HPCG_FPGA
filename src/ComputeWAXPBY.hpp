2a39e63d8d26175b0806a15ef9e39b36d853e13a
albertoz <albertoz@xilinx.com>
2020-01-28 16:56:38 +0000

    fixed flags, now working with 2 kernels

--- src/ComputeWAXPBY.hpp
+++ src/ComputeWAXPBY.hpp
@@ -15,6 +15,7 @@
 #ifndef COMPUTEWAXPBY_HPP
 #define COMPUTEWAXPBY_HPP
 #include "Vector.hpp"
+
 int ComputeWAXPBY(const local_int_t n, const double alpha, const Vector & x,
     const double beta, const Vector & y, Vector & w, bool & isOptimized);
 #endif // COMPUTEWAXPBY_HPP
