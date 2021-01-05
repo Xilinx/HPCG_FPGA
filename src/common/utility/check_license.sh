0c2a1f7914a99c9e4c25df2d2ab75802a3809677
Ken O'Brien <kennetho@xilinx.com>
2021-01-05 15:56:38 +0000

    licenses

--- src/common/utility/check_license.sh
+++ src/common/utility/check_license.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
 
+
+#
+# Copyright (C) 2021 Xilinx, Inc
+#
+# Licensed under the Apache License, Version 2.0 (the "License"). You may
+# not use this file except in compliance with the License. A copy of the
+# License is located at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+#
+
 # Check if LICENSE.txt is provided as an argument
 
 if [ $# -eq 0 ]; then
